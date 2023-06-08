1a19bb58659a3ae9e43eca47956fa52d67487bed
Jack Windham <jckwind11@gmail.com>
2023-06-08 07:17:35 -0400

    Initial Commit

---
+++ Ledger/LedgerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LedgerApp.swift
+//  Ledger
+//
+//  Created by Jack Windham on 6/8/23.
+//
+
+import SwiftUI
+
+@main
+struct LedgerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
